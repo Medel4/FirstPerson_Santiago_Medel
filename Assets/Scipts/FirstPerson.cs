14873157751f7b6273b95d7b9c0f08c7a1702171
Medel4 <santiagomedelmarcos@gmail.com>
2024-10-17 21:15:46 +0200

    :)

---
+++ Assets/Scipts/FirstPerson.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] 
+    CharacterController cc;
+    void Start()
+    {
+
+        cc = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 input = new Vector2(h, v).normalized;
+
+        if (input.magnitude > 0)
+        {
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+            Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
+            cc.Move( movimiento * velocidadMovimiento * Time.deltaTime);
+        }
+        
+        
+        
+    }
+}
