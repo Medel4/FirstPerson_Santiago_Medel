1f68f033748ceeedab90441cd564af9a856ca8a2
Medel4 <santiagomedelmarcos@gmail.com>
2024-10-21 17:46:47 +0200

    saf

--- Assets/Scipts/ThirdPerson.cs
+++ Assets/Scipts/ThirdPerson.cs
@@ -8,6 +8,7 @@ public class ThirdPerson : MonoBehaviour
     [SerializeField] private float velocidadMovimiento;
     [SerializeField] private float smoothing;
     private float velocidadRotacion;
+    private Animator anim;
 
     CharacterController cc;
     void Start()
@@ -15,28 +16,40 @@ public class ThirdPerson : MonoBehaviour
 
         cc = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+        anim = GetComponent<Animator>();
 
     }
 
 
     void Update()
     {
+        Movimiento();
+
+    }
+
+    private void Movimiento()
+    {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
         Vector2 input = new Vector2(h, v).normalized;
 
+
+
         if (input.magnitude > 0)
         {
             float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-            Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloRotacion, ref velocidadRotacion, smoothing);
-            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+            float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloRotacion, ref velocidadRotacion, smoothing);
+            transform.eulerAngles = new Vector3(0, anguloSuave, 0);
 
             Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
             cc.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+
+            anim.SetBool("isRunning", true);
         }
-
-
-
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
     }
 }
